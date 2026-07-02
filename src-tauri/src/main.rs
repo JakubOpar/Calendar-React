b5b8af75f2d389fc88e52af34e62c0dffb6738bf
Jakub <oparkuba2002@gmail.com>
2026-07-02 10:43:04 +0200

    project prepare, add sqlite connection

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,23 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri_plugin_sql::Builder as SqlBuilder;
+
+#[tauri::command]
+fn db_test() -> String {
+  "SQLite backend connected".into()
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+  tauri::Builder::default()
+    .plugin(SqlBuilder::default().build())
+    .invoke_handler(tauri::generate_handler![db_test])
+    .setup(|_app| {
+      println!("App started");
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
 
 fn main() {
-  app_lib::run();
+  run();
 }
